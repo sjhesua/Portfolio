a4323f2f107b8a4908fbd8acd5ba3e26ef6afa0b
sjhesua <sjhesua@gmail.com>
2025-10-04 21:40:24 -0400

    redirectToDefaultLocale: false

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     locales: ['en', 'es'],
     routing: {
       prefixDefaultLocale: true,
-      redirectToDefaultLocale: true,
+      redirectToDefaultLocale: false,
     }
   },
 });
